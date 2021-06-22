b502d361f28550f34cc961c590285cb1b8851b1d
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-06-22 12:42:37 +0530

    theme switching

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
+import { ThemeProvider } from "styled-components"
+import { useState, useEffect } from "react"
+import { lightTheme, darkTheme, GlobalStyles } from "../ThemeConfig"
+import Layout from '../Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [theme, setTheme] = useState("dark")
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme])
+
+  const toggleTheme = () => {
+    theme == 'light' ? setTheme('dark') : setTheme('light')
+  }
+
+  return (
+    <ThemeProvider theme={theme == 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <button onClick={toggleTheme}>Switch {theme}</button>
+      <Layout>
+        <Component {...pageProps} theme={theme} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
