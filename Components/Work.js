6015e035c0ae65db1a1a9ee04b60aa16b4a952d3
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-08 01:16:23 +0530

    work section

--- Components/Work.js
+++ Components/Work.js
@@ -1,11 +1,22 @@
 import styles from '../styles/Work.module.css'
 import WorkProject from './WorkProject'
+import { projects } from '../Collections/projects'
 
 const Work = ({ currentTheme }) => {
     return (
         <div>
            <div className={styles.workHeading}>Projects and Blogs</div>
-           <WorkProject />
+           <div className={styles.workmain} style={{color: currentTheme.subtext}}>
+              {
+                  projects.map((project) => {
+                      return (
+                          <div key={project.id}>
+                              <WorkProject currentTheme={currentTheme} project={project}/>
+                          </div>
+                      )
+                  })
+              }
+           </div>
         </div>
     )
 }
--- Components/WorkProject.js
+++ Components/WorkProject.js
@@ -3,44 +3,66 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubAlt, faLinkedinIn, faTelegramPlane } from "@fortawesome/free-brands-svg-icons"
 import Link from 'next/link'
 import { HStack, Tag, TagLabel } from "@chakra-ui/react"
+import { useToast } from "@chakra-ui/react"
 
-const WorkProject = ({ currentTheme }) => {
+const WorkProject = ({ currentTheme, project }) => {
+  const toast = useToast()
+
+  const showGitToast = () => {
+    toast({
+          description: "Sorry, this project has a private repository!",
+          status: "info",
+          isClosable: true,
+        })
+  }
+
+  const showProjToast = () => {
+    toast({
+          description: "Sorry, this project has not been deployed yet!",
+          status: "info",
+          isClosable: true,
+        })
+  }
+
     return (
-        <div className={styles.parentcard}>
-            <div className={styles.cardtitle}>
-                <h1 className={styles.projtitle}>Project title</h1>
-                <HStack spacing={2}>
-                    <Tag size="md" borderRadius="md" variant="subtle" colorScheme="cyan">
-                        <TagLabel>Green</TagLabel>
-                    </Tag>
-                    <Tag size="md" borderRadius="md" variant="subtle" colorScheme="cyan">
-                        <TagLabel>Green</TagLabel>
-                    </Tag>
-                    <Tag size="md" borderRadius="md" variant="subtle" colorScheme="cyan">
-                        <TagLabel>Green</TagLabel>
-                    </Tag>
-                </HStack>
-            </div>
-            <div className={styles.imageandsocials}>
-                <div className={styles.card}>
-                    <div className={styles.arrows}>
-                        <p onClick={() => alert('clicked back')}>&lt;</p>
-                        <p onClick={() => alert('clicked ahead')}>&gt;</p>
-                    </div>
-                </div>
-                <div>
-                    <div className={styles.socialIcon}>
-                        <Link href="https://github.com/AsavariA"><a><FontAwesomeIcon icon={faGithubAlt} /></a></Link>
-                    </div>
-                    <div className={styles.socialIcon}>
-                        <Link href="https://www.linkedin.com/in/asavariambavane/"><a><FontAwesomeIcon icon={faTelegramPlane} /></a></Link>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.cardbottom}>
-                <p className={styles.projdescription}>A Commercial Website for the branding agency, INZA Design Co. Seamlt an aesthetic feel to it.</p>
-                <div className={styles.cta}><i>Check it out &rarr;</i></div>
-            </div>
+        <div className={styles.parentofparentcard} style={{justifyContent: project.justifyContent}}>
+          <div className={styles.parentcard}>
+              <div className={styles.cardtitle}>
+                  <h1 className={styles.projtitle}>{project.name}</h1>
+                  <HStack spacing={2}>
+                    {
+                      project.tech.map((tech) => {
+                          return (
+                              <div key={tech}>
+                                <Tag size="sm" borderRadius="md" variant="subtle" colorScheme={ currentTheme.name === 'light' ? "cyan" : "purple"}>
+                                    <TagLabel>{tech}</TagLabel>
+                                </Tag>
+                              </div>
+                          )
+                      })
+                    }
+                  </HStack>
+              </div>
+              <div className={styles.imageandsocials}>
+                  <div className={styles.card} style={{borderColor: currentTheme.text, backgroundImage: `url('../styles/NPM/1.jpg')`}}>
+                      <div className={styles.arrows} style={{color: 'black'}}>
+                          <p onClick={() => alert('clicked back')}>&lt;</p>
+                          <p onClick={() => alert('clicked ahead')}>&gt;</p>
+                      </div>
+                  </div>
+                  <div>
+                      <div className={styles.socialIcon} style={{color: currentTheme.subtext, borderColor: currentTheme.subtext}}>
+                          {project.githubLink !== '' ? <Link href={project.githubLink}><a><FontAwesomeIcon icon={faGithubAlt} /></a></Link> : <FontAwesomeIcon icon={faGithubAlt} onClick={showGitToast}/>}
+                      </div>
+                      <div className={styles.socialIcon} style={{color: currentTheme.subtext, borderColor: currentTheme.subtext}}>
+                          {project.projectLink !== '' ? <Link href={project.projectLink}><a><FontAwesomeIcon icon={faTelegramPlane} /></a></Link> : <FontAwesomeIcon icon={faTelegramPlane} onClick={showProjToast}/>}
+                      </div>
+                  </div>
+              </div>
+              <div className={styles.cardbottom}>
+                  <p className={styles.projdescription}>{project.description}</p>
+              </div>
+          </div>
         </div>
     )
 }
