b2c8e94297bf659165c82d3a5723c3cb2c4ec9e7
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-09 02:33:30 +0530

    blog cards and more

--- Components/Work.js
+++ Components/Work.js
@@ -1,22 +1,41 @@
 import styles from '../styles/Work.module.css'
 import WorkProject from './WorkProject'
+import BlogCard from './BlogCard';
 import { projects } from '../Collections/projects'
+import { useState } from 'react';
 
 const Work = ({ currentTheme }) => {
+
+    const [blogList, setBlogList] = useState([])
+
+fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ambavaneasavari')
+        .then(response => response.json())
+        .then(data => setBlogList(data.items))
+        .catch(err => console.error(err));
+
     return (
         <div>
-           <div className={styles.workHeading}>Projects and Blogs</div>
-           <div className={styles.workmain} style={{color: currentTheme.subtext}}>
-              {
-                  projects.map((project) => {
-                      return (
-                          <div key={project.id}>
-                              <WorkProject currentTheme={currentTheme} project={project}/>
-                          </div>
-                      )
-                  })
-              }
-           </div>
+            <div className={styles.workHeading}>Projects and Blogs</div>
+            <div className={styles.workmain} style={{ color: currentTheme.subtext }}>
+                {
+                    projects.map((project) => {
+                        return (
+                            <div key={project.id}>
+                                <WorkProject currentTheme={currentTheme} project={project} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            { blogList ? 
+                    blogList.map((blog, key) => {
+                        return (
+                            <div key={key}>
+                                <BlogCard blog={blog} currentTheme={currentTheme}/>
+                            </div>
+                        )
+                    }) : <div>Loading...</div>
+                }
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn-images-1.medium.com'],
+  },
 }
