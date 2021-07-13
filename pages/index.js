a6bc04387b466ab5c4b8f3c163b70e94807f3c91
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-13 16:01:52 +0530

    meta image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import HomePage from '../Components/HomePage'
-import metaImage from '../styles/meta-image.png'
 
 export default function Home({ currentTheme }) {
 
@@ -15,12 +14,12 @@ export default function Home({ currentTheme }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Asavari Ambavane" />
         <meta property="og:description" content="Hello, I am a full-stack web developer and this is my portfolio :)" />
-        <meta property="og:image" content={metaImage} />
+        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/daily-productivity.appspot.com/o/meta1.PNG?alt=media&token=17ee3549-9296-4cb0-b1e0-5e6fd933f9b3" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content="Asavari Ambavane" />
         <meta property="twitter:description" content="Hello, I am a full-stack web developer and this is my portfolio :)" />
-        <meta property="twitter:image" content={metaImage} />
+        <meta property="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/daily-productivity.appspot.com/o/meta1.PNG?alt=media&token=17ee3549-9296-4cb0-b1e0-5e6fd933f9b3" />
       </Head>
       <HomePage currentTheme={currentTheme} />
     </div>
