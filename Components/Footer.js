098e20e75715af6c21fe43cd91b82268258549dd
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-31 13:48:44 +0530

    footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,9 +1,33 @@
 import styles from '../styles/NavbarFooter.module.css';
+import { userinfo } from '../Constants/userinfo'
+import Link from 'next/link'
 
-const Footer = ({currentTheme}) => {
+const Footer = ({ currentTheme }) => {
     return (
-        <div className={styles.footermain} style={{backgroundColor: currentTheme.footerColor}}>
-            <h2 className={styles.footercontent}>Asavari Ambavane | Copyright © 2021</h2>
+        <div className={styles.footermain} style={{ backgroundColor: currentTheme.footerColor, color: currentTheme.subtext }}>
+            <div className={styles.footertable}>
+                <Link href='/'><a><h2 className={styles.footerlogo}>{userinfo.logoText}</h2></a></Link>
+                <ul>
+                    <li className={styles.listHeading}>Socials</li>
+                    {userinfo.socials ?
+                        userinfo.socials.map((social, key) => {
+                            return (
+                                <Link href={social.link} key={key}><a><li>{social.type}</li></a></Link>
+                            )
+                        }) : null
+                    }
+                    <Link href={`mailto:${userinfo.contact.email ? userinfo.contact.email : ''}`}><a><li>Mail</li></a></Link>
+                </ul>
+                <ul>
+                    <li className={styles.listHeading}>Pages</li>
+                    <Link href='/'><a><li>Home</li></a></Link>
+                    <Link href='/#about'><a><li>About</li></a></Link>
+                    <Link href='/work'><a><li>Work</li></a></Link>
+                    <Link href='/contact'><a><li>Contact</li></a></Link>
+                </ul>
+            </div>
+            <hr style={{ height: '1px', backgroundColor: currentTheme.subtext, border: 'none', opacity: '0.5' }}></hr>
+            <h2 className={styles.footercontent}>Template by Asavari Ambavane</h2>
         </div>
     )
 }
