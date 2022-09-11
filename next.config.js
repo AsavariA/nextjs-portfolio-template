811385b7126f4181f2e8ad76b1f8da9319565f80
Bhathiya Prasad <90783913+BhathiyaRathnayake@users.noreply.github.com>
2022-09-11 17:41:07 +0530

    Prepare for Firebase hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
+  swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
   },
 }
